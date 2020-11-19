f4553c7e67087f484b924b02072d82e5802c9664
Nick Cabrera <nickcabrera75@gmail.com>
2020-11-19 10:53:23 -0400

    Agrego Splash Screen y Modifico icono de la aplicacion

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ProyectoFinal2020
 Proyecto Final Movil 2
+Se agrego Splash Screen y se modifico el icono del la aplicacion
--- app/src/main/java/com/example/proyectofinal2020/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal2020/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.proyectofinal2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //ANIMACIONES SPLASH SCREEN
+        Animation animArriba = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animAbajo = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        ImageView ivContacto = findViewById(R.id.ivContacto);
+        TextView tvContacto = findViewById(R.id.tvContacto);
+        ImageView ivIntegrantes = findViewById(R.id.ivIntegrantes);
+        TextView tvIntegrantes = findViewById(R.id.tvIntegrantes);
+        ImageView ivLogo = findViewById(R.id.ivLogo);
+
+        ivContacto.setAnimation(animAbajo);
+        tvContacto.setAnimation(animAbajo);
+        ivIntegrantes.setAnimation(animAbajo);
+        tvIntegrantes.setAnimation(animAbajo);
+        ivLogo.setAnimation(animArriba);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
 }
